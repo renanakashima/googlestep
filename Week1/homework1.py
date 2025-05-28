468ea70ca111d4c0fa241c6915cec5e9838fc7ef
renanakashima <renanakashima@g.ecc.u-tokyo.ac.jp>
2025-05-28 15:02:17 +0200

    modified

--- Week1/homework1.py
+++ Week1/homework1.py
@@ -12,6 +12,32 @@ def txt_to_list(txt_file):
         lis = [line.rstrip() for line in f]
     return lis
 
+
+# n items in a list
+# sorting: O(n * log n) time
+# loop: O(n) time
+
+
+def find_anagrams(string, dictionary):
+    # O(1) constant time
+    string = ''.join(sorted(string))
+
+    new_dict = []
+    # O(1 * n), where n is the number of words in dictionary
+    # O(n) time
+    for word in dictionary:
+        # O(1)
+        new_dict.append([''.join(sorted(word)), word]) 
+    # O(n * log n)
+    new_dict.sort()
+
+    # binary search takes O(log n) time
+    # worst cast: n matches, O(n) time
+    # O(log n  + n) time => O(n) time
+    for index in binary_search(string,new_dict):
+        print(new_dict[index][1])
+
+"""
 def find_anagrams(random_strings, dictionary):
     strings = []
     for s in random_strings:
@@ -31,10 +57,11 @@ def find_anagrams(random_strings, dictionary):
         
     print(full_list)
     return full_list
-
+"""
 def main():
-    find_anagrams(txt_to_list(test_cases),txt_to_list(dictionary_with_words))
-
+    #find_anagrams(txt_to_list(test_cases),txt_to_list(dictionary_with_words))
+    find_anagrams("board",txt_to_list(dictionary_with_words))
+    
 if __name__ == "__main__":
     main()
     
